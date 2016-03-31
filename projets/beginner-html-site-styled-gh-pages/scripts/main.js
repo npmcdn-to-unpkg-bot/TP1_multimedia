5fb5d040f759b770faee18ca29094d6a845fc0f4
FabienneMarquis <fabienne.marquis@gmail.com>
2016-03-31 16:10:55 -0400

    done tp1

--- projets/beginner-html-site-styled-gh-pages/scripts/main.js
+++ projets/beginner-html-site-styled-gh-pages/scripts/main.js
@@ -3,3 +3,32 @@
  */
 var monTitre = document.querySelector('h1');
 monTitre.textContent='Hello world!';
+
+var monImage = document.querySelector('img');
+
+monImage.onclick = function() {
+    var maSrc = monImage.getAttribute('src');
+    if(maSrc === 'images/firefox-icon.png') {
+        monImage.setAttribute ('src','images/fabienneShika.jpg');
+    } else {
+        monImage.setAttribute ('src','images/firefox-icon.png');
+    }
+}
+var monBouton = document.querySelector('button');
+var monTitre = document.querySelector('h1');
+
+function définirNomUtilisateur() {
+    var monNom = prompt('Veuillez saisir votre nom.');
+    localStorage.setItem('nom', monNom);
+    monTitre.textContent = 'Mozilla est cool, ' + monNom;
+}
+if(!localStorage.getItem('nom')) {
+    définirNomUtilisateur();
+} else {
+    var nomEnregistré = localStorage.getItem('nom');
+    monTitre.textContent = 'Mozilla est cool, ' + nomEnregistré;
+}
+
+monBouton.onclick = function() {
+    définirNomUtilisateur();
+}
