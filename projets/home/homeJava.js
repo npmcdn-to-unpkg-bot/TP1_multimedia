36f2698ba4c7cad200df0f22ecd6e352e32ab6ac
FabienneMarquis <fabienne.marquis@gmail.com>
2016-04-11 15:54:48 -0400

    abc tp2

---
+++ projets/home/homeJava.js
@@ -0,0 +1,15 @@
+/**
+ * Created by 1494778 on 2016-04-11.
+ */
+(function(){
+    var bCheck = document.getElementById("checkBsoudine");
+    var audio = document.getElementById("motBienvenu");
+    if(bcheck.checked){
+
+    }else if(!bCheck.checked){
+        
+    }
+    var foo = function() {};
+    window.onload = foo;
+
+})();
