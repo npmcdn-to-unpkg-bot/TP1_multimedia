516cb54d9e89f4f54bba0bdf1a8d2a797089f7ce
FabienneMarquis <fabienne.marquis@gmail.com>
2016-04-13 16:07:36 -0400

    abc tp2

--- projets/home/homeJava.js
+++ projets/home/homeJava.js
@@ -1,15 +1,44 @@
 /**
  * Created by 1494778 on 2016-04-11.
  */
-(function(){
-    var bCheck = document.getElementById("checkBsoudine");
-    var audio = document.getElementById("motBienvenu");
-    if(bcheck.checked){
-
-    }else if(!bCheck.checked){
-        
-    }
-    var foo = function() {};
+(function () {
+    "use strict";
     window.onload = foo;
+    window.onload = lienPage();
 
+    function foo() {
+        var bCheck = document.getElementById("checkBsoudine");
+        var audio = document.getElementById("motBienvenu");
+        if (bcheck.checked) {
+            $('#motBienvenu').pause();
+        } else if (!bCheck.checked) {
+            $('#motBienvenu').play();
+        }
+    };
+
+    function lienPage() {
+        var projets = [
+            {
+                nom: "Langues",
+                dir: "langues"
+            },
+            {
+                nom: "Pens",
+                dir: "pens"
+            },
+            {
+                nom: "Tutoriel",
+                dir: "mozilla"
+            },
+            {
+                nom: "Youtube",
+                dir: "youtube"
+            }
+        ];
+        for (var i = 0; i < projets.length; i++) {
+            document.getElementById('ulMenu').innerHTML = '<li><a  href="../'+projets[dir]+'/index.html">'+projets[nom]+'</a></li>';
+        }
+
+
+    };
 })();
