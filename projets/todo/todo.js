b8a542d1f843d597b38d732bd5bbc594709569c1
FabienneMarquis <fabienne.marquis@gmail.com>
2016-04-21 10:25:21 -0400

    tp2 fin D

--- projets/todo/todo.js
+++ projets/todo/todo.js
@@ -1,3 +1,219 @@
 /**
  * Created by 1494778 on 2016-04-14.
  */
+(function () {
+    "use strict";
+
+    var inputText = document.querySelector("#input"),
+        todolist = document.querySelector("#todolist"),
+        donelist = document.querySelector("#donelist"),
+        checkAllbBtn = document.querySelector("#checkAll"),
+        clearAllDoneBtn = document.querySelector("#clearAllDone");
+    getData();
+    should_mark_all_todo_btn_be_disabled();
+    should_clear_all_todo_btn_be_disabled();
+    dataUpdated();
+
+
+        inputText.onkeypress = function(e){
+        if (!e) e = window.event;
+        var keyCode = e.keyCode || e.which;
+        if (keyCode == '13'){
+            inputText.value.trim() != "" ? ajouterTodo(inputText.value.trim()) : "";
+            inputText.value = "";
+            return false;
+        }
+    };
+
+    function ajouterTodo( todoTexte ){
+        todoTexte = todoTexte || 'Chose à faire';
+        var todo = document.createElement("article");
+        todo.innerHTML = '<input type="checkbox" >' +
+            '<div tabindex="0" contenteditable="true">' + todoTexte + '</div>' +
+            '<img  src="../../common/images/close.png" alt="delete" tabindex="0" >';
+        var img = todo.querySelector('img'),
+            checkbox = todo.querySelector('input'),
+            div = todo.querySelector('div');
+        div.onkeypress = function (e) {
+            if (!e) e = window.event;
+            var keyCode = e.keyCode || e.which;
+            if (keyCode == '13') {
+                var sibling = this.parentNode.nextElementSibling;
+                if (sibling)
+                    sibling.querySelector("div").focus();
+                else if (this.parentNode.parentNode.nextElementSibling) {
+                    console.log((this.parentNode).parentNode.nextElementSibling);
+                    var article = this.parentNode.parentNode.nextElementSibling.querySelector("article");
+                    if (article) {
+                        article.querySelector("div").focus()
+                    } else {
+                        inputText.focus();
+                    }
+                } else
+                    inputText.focus();
+                return false;
+            }
+    };
+        div.onblur = function () {
+            dataUpdated();
+        };
+        checkbox.onchange = function () {
+            checkbox_onchange(this, todo);
+        };
+        checkbox.onkeypress = function (e) {
+            if (!e) e = window.event;
+            var keyCode = e.keyCode || e.which;
+            if (keyCode == '32') {
+                checkbox_onkeypress(this,todo);
+                return false;
+            }
+            if (keyCode == '13') {
+                checkbox_onkeypress(this,todo);
+                return false;
+            }
+
+        };
+        img.onclick = function () {
+            deleteTodo(this);
+        };
+        img.onkeypress = deleteTodoOnEnter;
+        todolist.insertBefore(todo, todolist.firstChild);
+
+        should_mark_all_todo_btn_be_disabled();
+        dataUpdated();
+        return todo;
+    }
+
+    function checkbox_onkeypress(checkbox,todo) {
+        checkbox.checked ? checkbox.checked = false : checkbox.checked = true;
+        checkbox_onchange(checkbox, todo);
+
+    }
+    function deleteTodo(node) {
+        var sibling = node.parentNode.nextElementSibling;
+        if (sibling)
+            sibling.querySelector("img").focus();
+        else if (node.parentNode.parentNode.nextElementSibling) {
+            //console.log((node.parentNode).parentNode.nextElementSibling);
+            var article = node.parentNode.parentNode.nextElementSibling.querySelector("article");
+            if (article) {
+                article.querySelector("img").focus()
+            } else {
+                if(node.parentNode.parentNode.childNodes.length >1){
+                    node.parentNode.parentNode.childNodes[node.parentNode.parentNode.childNodes.length-2].querySelector("img").focus();
+                }else{
+                    inputText.focus();
+                }
+            }
+        } else{
+            if(donelist.childNodes.length >1){
+                donelist.childNodes[donelist.childNodes.length-2].querySelector("img").focus();
+            }
+            else if(todolist.childNodes.length >1){
+                todolist.lastChild.querySelector("img").focus();
+            }else{
+                inputText.focus();
+            }
+
+        }
+
+        node.parentNode.outerHTML = "";
+
+
+        should_mark_all_todo_btn_be_disabled();
+        should_clear_all_todo_btn_be_disabled();
+        dataUpdated();
+    }
+
+    function deleteTodoOnEnter(e) {
+        if (!e) e = window.event;
+        var keyCode = e.keyCode || e.which;
+        if (keyCode == '13') {
+            deleteTodo(this);
+            return false;
+        }
+    }
+
+    function checkbox_onchange(checkbox, todo) {
+        if (checkbox.checked) {
+            donelist.insertBefore(todo, donelist.firstChild);
+        } else {
+            todolist.appendChild(todo);
+        }
+        dataUpdated();
+        should_mark_all_todo_btn_be_disabled();
+        should_clear_all_todo_btn_be_disabled();
+    }
+
+    function should_mark_all_todo_btn_be_disabled() {
+        if (todolist.childNodes.length == 0) {
+            checkAllbBtn.disabled = true;
+        } else {
+            checkAllbBtn.disabled = false;
+        }
+    }
+
+    function should_clear_all_todo_btn_be_disabled() {
+        if (donelist.childNodes.length == 0) {
+            clearAllDoneBtn.disabled = true;
+        } else {
+            clearAllDoneBtn.disabled = false;
+        }
+    }
+
+    checkAllbBtn.addEventListener("click", function () {
+        var checkboxes = todolist.querySelectorAll("input");
+        for (var i = 0; i < checkboxes.length; i++) {
+            checkboxes[i].checked = true;
+            checkbox_onchange(checkboxes[i], checkboxes[i].parentNode);
+        }
+    });
+    clearAllDoneBtn.onclick = function () {
+        var todos = donelist.querySelectorAll("input");
+        for (var i = 0; i < todos.length; i++) {
+            deleteTodo(todos[i]);
+        }
+    };
+    function dataUpdated() {
+        var json = {
+                todos: [],
+                doneTodos: []
+            },
+            todos = todolist.querySelectorAll("article"),
+            doneTodos = donelist.querySelectorAll("article"),
+            todo, i;
+        for (i = 0; i < todos.length; i++) {
+            todo = {checkbox: todos[i].querySelector("input").checked, text: todos[i].querySelector("div").textContent};
+            json.todos.push(todo);
+        }
+        for (i = 0; i < doneTodos.length; i++) {
+            todo = {
+                checkbox: doneTodos[i].querySelector("input").checked,
+                text: doneTodos[i].querySelector("div").textContent
+            };
+            json.doneTodos.push(todo);
+        }
+
+        localStorage.setItem("todos", JSON.stringify(json));
+    }
+
+    function getData() {
+        var json = JSON.parse(localStorage.getItem("todos")), i, todo;
+        if(json){
+            for (i = json.todos.length - 1; i >= 0; i--) {
+                todo = ajouterTodo(json.todos[i].text);
+
+            }
+            for (i = 0; i < json.doneTodos.length; i++) {
+                todo = ajouterTodo(json.doneTodos[i].text);
+                todo.querySelector("input").checked = true;
+                donelist.appendChild(todo);
+
+
+            }
+        }
+
+    }
+
+
+}) ();
