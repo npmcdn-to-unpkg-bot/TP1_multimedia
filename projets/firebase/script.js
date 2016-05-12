0f20014634f8fef259ec202c3920d952affbdfc1
FabienneMarquis <fabienne.marquis@gmail.com>
2016-05-12 12:45:37 -0400

    TPY firebase blu

--- projets/firebase/script.js
+++ projets/firebase/script.js
@@ -3,23 +3,67 @@
  */
 (function () {
     "use strict";
+    var counterMsg,
+        myDataRef = new Firebase('https://crackling-torch-8087.firebaseio.com//'),
+        inputMSG = $('#messageInput');
 
-    var myDataRef = new Firebase('https://crackling-torch-8087.firebaseio-demo.com/');
-    $('#messageInput').keypress(function (e) {
+    myDataRef.on('value', function (snapshot) {
+        console.log('dans snapshot');
+        counterMsg = 0;
+        snapshot.forEach(function () {
+            counterMsg++;
+            console.log('dans snapshot, compter');
+        });
+        if (counterMsg == 0) {
+            $('#countMsg').text(counterMsg + ' message');
+        } else {
+            $('#countMsg').text(counterMsg + ' messages');
+        }
+
+    });
+
+
+    $('#countMsg').click(function () {
+        for (var i = 1; i == 50; i++) {
+            var name = 'Anonymous';
+            var text = i;
+            myDataRef.push({name: name, text: text});
+        }
+    });
+
+    function clearMSG() {
+        myDataRef.remove(function (error) {
+            if (error) {
+                console.error("ERREUR: " + error);
+            } else {
+                console.log("toutes les données ont été effacées.");
+            }
+        });
+    };
+    inputMSG.keypress(function (e) {
         if (e.keyCode == 13) {
             var name = $('#nameInput').val();
             var text = $('#messageInput').val();
             myDataRef.push({name: name, text: text});
             $('#messageInput').val('');
+            localStorage.setItem("name", $('#nameInput').val() );
         }
     });
-    myDataRef.on('child_added', function(snapshot) {
+    myDataRef.on('child_removed', function (oldChildSnapshot) {
+
+    });
+
+    myDataRef.on('child_added', function (snapshot) {
         var message = snapshot.val();
         displayChatMessage(message.name, message.text);
+        $('#nameInput').text(localStorage.getItem("name"));
     });
+
     function displayChatMessage(name, text) {
-        $('<div/>').text(text).prepend($('<em/>').text(name+': ')).appendTo($('#messagesDiv'));
+        $('<div/>').text(text).prepend($('<em/>').text(name + ': ')).appendTo($('#messagesDiv'));
         $('#messagesDiv')[0].scrollTop = $('#messagesDiv')[0].scrollHeight;
     };
+
+
 
 })();
